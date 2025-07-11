211d7d3b86510ed9996aac862da7b5225afb6382
NunoF <n.fernandes.contact@gmail.com>
2025-07-11 17:10:38 +0200

    feat: Update password validation to require a minimum length of 8 characters

--- src/users/create-user.dto.ts
+++ src/users/create-user.dto.ts
@@ -9,6 +9,6 @@ export class CreateUserDto {
   email: string;
 
   @IsNotEmpty({ message: 'Password is required' })
-  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
   password: string;
 }
